d9354c1c3d11686063511a881e2ebf9aa181dddb
hideokawahara <kawahide.72@gmail.com>
2020-06-07 18:18:13 +0900

    change the comment's view

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,7 @@
       <!-- <img src='./assets/conver.png' class="logo"> -->
       <p class="username">あなたの名前: {{ username }}</p>
       <p class="online">今暇な人: {{ users.length }}</p>
+      
     </div>
     
       <ChatRoom v-bind:messages="messages" v-on:sendMessage="this.sendMessage" />
@@ -98,22 +99,31 @@ body {
   width: auto;
   height: auto;
   h1 {
-    color: rgb(9, 211, 26);
+    color: rgba(74, 248, 6, 0.733);
     text-shadow: 1px 3px rgba(0, 0, 0, 0.25); 
     text-align: center;
+    &:hover {
+				animation-name: rubberBand;
+        animation-duration: 0.5s;
+			}
   }
   // .logo {
   //  height: 200px;
   //  width: 400px;
   //  padding: 0;
   // }
-  // p {
-  //   // text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-  //   // color: #ffffff;
-  //   // font-weight: bold;
-  //   // box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-    
-  // }
+  p {
+    font-family: 'Lato', 'Noto Sans JP', 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+    display: inline-block;
+    background-color: rgba(187, 255, 0, 0.5);
+    box-shadow: inset 0 0 15px rgba(0,0,0,.3);
+    border-radius: 40px;
+    padding: 3px;
+   }
+  p.online {
+    margin-left: 30%;
+  }
+  
 }
 
 </style>
