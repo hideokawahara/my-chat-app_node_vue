3ea999584fc8a469128c8813affc83f1ef0660db
hideokawahara <kawahide.72@gmail.com>
2020-08-02 23:37:58 +0900

    make it pink theme

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
+   
     <div class="header">
       <h1 class="logo">conVer</h1>
-      <!-- <img src='./assets/conver.png' class="logo"> -->
       <p class="username">あなたの名前: {{ username }}</p>
       <p class="online">今暇な人: {{ users.length }}</p>
-      
+      <div class="heart"></div>
     </div>
-    
       <ChatRoom v-bind:messages="messages" v-on:sendMessage="this.sendMessage" />
-    
   </div>
 </template>
-
 <script>
 import io from 'socket.io-client';
 import ChatRoom from './components/ChatRoom';
-
-
 export default {
   name: 'App',
   components: {
@@ -38,7 +33,6 @@ export default {
 				this.users = data.users;
 				this.socket.emit('newuser', this.username);
       });
-
       this.listen();
     },
     listen: function () {
@@ -62,21 +56,19 @@ export default {
     if (!this.username) {
 			this.username = "ゲスト";
     }
-    
     this.joinServer();
   }
 }
 </script>
-
 <style lang="scss">
 body {
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	color: #2C3E50;
 	margin: 0;
 	padding: 0;
-  background-image: linear-gradient(to bottom right, rgb(241, 207, 233), rgb(170, 226, 243));
+  // background-image: linear-gradient(to bottom right, rgb(241, 207, 233), rgb(170, 226, 243));
+  background-image: linear-gradient(-45deg,#ffc2d2 0,#feceda 50%,#f890aa 100%);
 }
-
 #app {
 	display: flex;
 	flex-direction: column;
@@ -87,19 +79,18 @@ body {
   padding: 0 15px 15px;
   box-sizing: border-box;
 }
-
 .header {
-  // background-color: #ffffff;
-  // opacity: 0.5;
-  background: rgba(162, 233, 30, 0.26);
+  margin-top: 10px;
+  background-color: #fdedf1;
   margin-bottom: 20px;
   border-radius: 30px;
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  // box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  box-shadow: 3px 6px rgba(247, 72, 3, 0.25);
   padding: 10px;
   width: auto;
   height: auto;
   h1 {
-    color: rgba(74, 248, 6, 0.733);
+    color: #ff597e;
     text-shadow: 1px 3px rgba(0, 0, 0, 0.25); 
     text-align: center;
     &:hover {
@@ -107,23 +98,58 @@ body {
         animation-duration: 0.5s;
 			}
   }
-  // .logo {
-  //  height: 200px;
-  //  width: 400px;
-  //  padding: 0;
-  // }
   p {
     font-family: 'Lato', 'Noto Sans JP', 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
     display: inline-block;
-    background-color: rgba(187, 255, 0, 0.5);
-    box-shadow: inset 0 0 15px rgba(0,0,0,.3);
+    background-color: #f17091;
+    color: #fff;
+    // box-shadow: inset 0 0 15px rgba(0,0,0,.3);
+    // border-radius: 40px;
+    border: 2px solid #f17091;
     border-radius: 40px;
+    border: 2px solid #f17091;
     padding: 3px;
    }
   p.online {
     margin-left: 30%;
   }
-  
 }
 
+.heart {
+  position: absolute;
+  // width: 100px;
+  // height: 90px;
+  width: 100px;
+  height: 90px;
+  left: 75%;
+  margin-top: -45px;
+  margin-left: -50px;
+}
+.heart:before,
+.heart:after {
+  position: absolute;
+  content: "";
+  left: 50px;
+  top: 0;
+  width: 50px;
+  height: 80px;
+  background: #f78cc7f3;
+  border-radius: 50px 50px 0 0;
+  transform: rotate(-45deg);
+  transform-origin: 0 100%;
+}
+.heart:after {
+  left: 0;
+  transform: rotate(45deg);
+  transform-origin :100% 100%;
+}
+
+.heart {
+  animation: pounding .5s linear infinite alternate;
+}
+ 
+@keyframes pounding{
+  0%{ transform: scale(1.5); }
+  100%{ transform: scale(1); }
+}
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-// import VueScrollTo from 'vue-scrollto'
-// var VueScrollTo = require('vue-scrollto');
-
-
 
 Vue.use(BootstrapVue)
-// Vue.use(VueScrollTo)
 
 Vue.config.productionTip = false
 
